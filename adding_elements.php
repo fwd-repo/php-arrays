b0b4d08d1b7e016896372cf3379ce9e384597872
Ed Tansey <office.fwd@yahoo.com>
2021-08-17 00:16:44 +0100

    Add adding elements file

---
+++ adding_elements.php
@@ -0,0 +1,9 @@
+<?php
+
+$learn = array('Conditionals','Arrays','Loops');
+$learn[] = 'Build something awesome!';
+array_push($learn,'Functions','Forms','Objects');
+array_unshift($learn,'HTML','CSS');
+var_dump($learn);
+
+?>
