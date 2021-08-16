d2d8da15ffa488e1b61a780de113262e57ba660e
Ed Tansey <office.fwd@yahoo.com>
2021-08-17 00:28:05 +0100

    Add associative array file

---
+++ associative_array.php
@@ -0,0 +1,13 @@
+<?php
+
+$iceCream = array(
+  'Alena' => 'Black Cherry', 
+  'Treasure' => 'Chocolate', 
+  'Dave' => 'Cookies and Cream', 
+  'Rialla' => 'Strawberry'
+);
+
+echo $iceCream['Alena'];
+var_dump($iceCream);
+
+?>
